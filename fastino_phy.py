3e32d30a1cb9a634f0ae641b56cccb0ab26a4e80
Robert Jördens <rj@quartiq.de>
2020-01-11 21:50:58 +0000

    no CE since it sees falling edge first

--- fastino_phy.py
+++ fastino_phy.py
@@ -337,7 +337,7 @@ class MultiSPI(Module):
         sr = [Signal(n_bits, reset_less=True) for i in range(n_channels)]
         # SPI bus enable bits
         enable = Signal(n_channels, reset_less=True)
-        assert len(Cat(sr, enable)) == n
+        assert len(Cat(enable, sr)) == n
 
         self.sync.spi += [
             [sri[1:].eq(sri) for sri in sr],  # MSB first
@@ -346,10 +346,9 @@ class MultiSPI(Module):
             ).Elif(self.stb,
                 self.busy.eq(1),
                 Cat(enable, sr).eq(self.data),
-            ).Else(
-                enable.eq(0),
             ),
             If(i == n_bits - 1,
+                enable.eq(0),
                 self.busy.eq(0),
             ),
         ]
@@ -357,29 +356,29 @@ class MultiSPI(Module):
             self.specials += [
                 Instance(
                     "SB_IO",
-                    p_PIN_TYPE=C(0b110000, 6),  # output registered DDR
+                    p_PIN_TYPE=C(0b010000, 6),  # output registered DDR
                     p_IO_STANDARD="SB_LVCMOS",
                     i_OUTPUT_CLK=ClockSignal("spi"),
-                    i_CLOCK_ENABLE=enable[i],
+                    #i_CLOCK_ENABLE=enable[i],
                     o_PACKAGE_PIN=spi[i].clk,
                     i_D_OUT_0=0,
-                    i_D_OUT_1=self.busy),
+                    i_D_OUT_1=enable[i]),
                 Instance(
                     "SB_IO",
-                    p_PIN_TYPE=C(0b110100, 6),  # output registered
+                    p_PIN_TYPE=C(0b010100, 6),  # output registered
                     p_IO_STANDARD="SB_LVCMOS",
                     i_OUTPUT_CLK=ClockSignal("spi"),
-                    i_CLOCK_ENABLE=enable[i],
+                    #i_CLOCK_ENABLE=enable[i],
                     o_PACKAGE_PIN=spi[i].sdi,
-                    i_D_OUT_0=sr[i][-1]),
+                    i_D_OUT_0=sr[i][-1] & enable[i]),
                 Instance(
                     "SB_IO",
-                    p_PIN_TYPE=C(0b111100, 6),  # output registered inverted
+                    p_PIN_TYPE=C(0b011100, 6),  # output registered inverted
                     p_IO_STANDARD="SB_LVCMOS",
                     i_OUTPUT_CLK=ClockSignal("spi"),
-                    i_CLOCK_ENABLE=enable[i],
+                    #i_CLOCK_ENABLE=enable[i],
                     o_PACKAGE_PIN=spi[i].nss,
-                    i_D_OUT_0=self.busy),
+                    i_D_OUT_0=enable[i]),
             ]
             # With LDAC tied permanently low, the rising edge of
             # CS loads the data to the DAC.
